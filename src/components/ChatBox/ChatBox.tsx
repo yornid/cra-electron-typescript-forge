f8d88f4b460c2b245e4a15a893cae0a81a513f22
Ronald <yornid@gmail.com>
2021-05-13 18:36:13 +0800

    test2

--- src/components/ChatBox/ChatBox.tsx
+++ src/components/ChatBox/ChatBox.tsx
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react'
-
-import Dialogs from './Dialogs'
-import { MyWordHOC } from './MyWord'
-
-import s from './chatBox.module.css'
+import React, { useEffect } from "react";
+import s from "./chatBox.module.css";
+import Dialogs from "./Dialogs";
+import { MyWordHOC } from "./MyWord";
 
 const t: number = 5000;
 
+// add comment on ChatBox
 function ChatBox({ Chat }: any) {
   useEffect(() => {
-    const tId = setInterval(Chat.getLatestDialogs, t)
-    return () => clearInterval(tId)
-  }, [Chat])
+    const tId = setInterval(Chat.getLatestDialogs, t);
+    return () => clearInterval(tId);
+  }, [Chat]);
   return (
     <div className={s.chatBox}>
       <Dialogs dialogs={Chat.dialogs} />
       <MyWordHOC Chat={Chat} />
     </div>
-  )
+  );
 }
 
-export default ChatBox
+export default ChatBox;
--- src/components/FeedBox/FeedBox.tsx
+++ src/components/FeedBox/FeedBox.tsx
@@ -1,33 +1,36 @@
-import React, { useEffect } from 'react'
-import { observer } from 'mobx-react-lite'
-import classnames from 'classnames'
-import axios from 'axios'
+import axios from "axios";
+import classnames from "classnames";
+import { observer } from "mobx-react-lite";
+import React, { useEffect } from "react";
+import { FeedType } from "../../StoreProvider";
+import { randomFeeds } from "../../utils";
+import s from "./feedBox.module.css";
 
-import { FeedType } from '../../StoreProvider'
-import { randomFeeds } from '../../utils'
+const t: number = 5000;
 
-import s from './feedBox.module.css'
-
-const t: number = 5000
-
+// add commnet on FeedBox
 function FeedBox({ Feed }: any) {
   useEffect(() => {
     async function loop() {
       try {
-        await axios.get('/feeds')
+        await axios.get("/feeds");
       } catch (e) {
-        Feed.updateFeeds(randomFeeds())
+        Feed.updateFeeds(randomFeeds());
       }
     }
-    loop()
-    const tId = setInterval(loop, t)
-    return () => clearInterval(tId)
-  }, [Feed])
+    loop();
+    const tId = setInterval(loop, t);
+    return () => clearInterval(tId);
+  }, [Feed]);
   return (
     <ul className={s.feedBox}>
-      { Feed.feeds.map((item: FeedType) => (
+      {Feed.feeds.map((item: FeedType) => (
         <li className={s.wrap} key={item.id}>
-          <img className={classnames('img', s.img)} src={item.imgURL} alt={item.id} />
+          <img
+            className={classnames("img", s.img)}
+            src={item.imgURL}
+            alt={item.id}
+          />
           <div className={s.right}>
             <h4 className={s.break}>{item.title}</h4>
             <small className={s.break}>{item.desc}</small>
@@ -35,7 +38,7 @@ function FeedBox({ Feed }: any) {
         </li>
       ))}
     </ul>
-  )
+  );
 }
 
-export default observer(FeedBox)
+export default observer(FeedBox);
